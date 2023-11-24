78772ba2242887263afa2accf8c78420aebd6af7
releaser <herminiohenriques@gmail.com>
2023-11-24 21:08:42 +0000

    supabse authentication

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { AuthButtonServer } from "@/app/components/auth-button-server";
+
+function Login() {
+  return (
+    <section className="grid place-content-center min-h-screen">
+      <h1 className="text-xl font-bold mb-4">Inicia sesión en DevTer</h1>
+      <AuthButtonServer />
+    </section>
+  );
+}
+
+export default Login;
